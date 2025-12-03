2d379a61ac2e8815f9c11552fc1d8d08461282f0
liuyichun <liu_yichun@163.com>
2025-12-03 22:45:46 +0800

    调度中心修改为xxl-job最新版本支持ai功能

--- opentcs-extends/opentcs-job-server/src/main/java/org/opentcs/job/XxlJobServerApplication.java
+++ opentcs-extends/opentcs-job-server/src/main/java/org/opentcs/job/XxlJobServerApplication.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * SnailJob Server 启动程序
+ * XXL-JOB Server 启动程序
  *
  * @author opensnail
  * @date 2024-05-17
  */
 @SpringBootApplication
-public class SnailJobServerApplication {
+public class XxlJobServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(com.aizuda.snailjob.server.SnailJobServerApplication.class, args);
+        SpringApplication.run(XxlJobServerApplication.class, args);
     }
 
 }
